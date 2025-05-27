405e52829bbb4d601b85f02e38b5ebaab459f4f1
Azbito <azbitomsg@proton.me>
2025-05-26 23:50:40 -0300

    style: vitor's picture

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,19 +1,24 @@
 import { profile } from "@/data/profile";
+import Image, { StaticImageData } from "next/image";
 
 export function Intro() {
   return (
     <div className="h-[40rem] w-full overflow-hidden relative">
       <div className="flex items-center justify-center flex-col">
-        <h1 className="mt-32 text-primary text-center font-bold uppercase tracking-widest text-6xl">
+        <h1 className="mt-32 max-[860px]:mt-16 text-primary text-center font-bold uppercase tracking-widest text-6xl">
           {profile.role}
         </h1>
         <h2 className="text-6xl">{profile.name}</h2>
         <div className="absolute bottom-0 h-[18rem] w-[35rem] bg-primary rounded-t-full z-0" />
-        <img
-          className="h-[25rem] absolute bottom-0 z-10"
-          src={profile.image}
-          alt=""
-        />
+        <div className="absolute w-130 h-130 left-[50%] -translate-x-[50%] bottom-0">
+          <Image
+            className="relative z-10"
+            layout="fill"
+            objectFit="contain"
+            src={profile.image as StaticImageData}
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,7 +1,9 @@
+import Me from "@/assets/me/1.png"
+
 export const profile = {
   name: "Vitor Schultz",
   role: "Marketing & Growth",
-  image: "https://pngimg.com/d/man_PNG6531.png",
+  image: Me,
   about:
     "Profissional de Marketing Digital <highlight>com mais de 2 anos de experiência</highlight> em SEO, tráfego pago, CRM, análise de dados, Inbound Marketing, criação e estratégia de conteúdo. Atuação na otimização de sites, execução de campanhas de marketing digital e desenvolvimento de estratégias para melhorar a performance orgânica e paga. Conhecimento em ferramentas como Google Search Console, Google Analytics, Google Ads, Meta Ads, RD Station CRM e gerenciamento de tags via Google Tag Manager (GTM). Experiência em auditoria técnica de websites e social media, além da criação de processos internos para otimizar rotinas e apoiar a gestão de mídias digitais. Atualmente, focado no aprimoramento em branding, automação de marketing e Growth Marketing.",
   years: 2,
